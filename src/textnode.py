b32fa02eb69613c5f5e15a5011e04760d71541a5
Daniel-Parada-da-Silva <daniel.parada.da.silva@gmail.com>
2025-05-13 11:01:13 +0200

    add test.sh

--- src/textnode.py
+++ src/textnode.py
@@ -1,18 +1,20 @@
 from enum import Enum
 
 class TextType(Enum):
-    NORMAL_TEXT = "Normal"
-    BOLD_TEXT = "Bold"
-    ITALIC_TEXT = "Italic"
-    CODE_TEXT = "Code"
-    LINKS = "Link"
-    IMAGES = "Image"
+    NORMAL = "Normal"
+    BOLD = "Bold"
+    ITALIC = "Italic"
+    CODE = "Code"
+    LINK = "Link"
+    IMAGE = "Image"
 
 class TextNode():
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url = None):
         self.text = text
         self.text_type = text_type
         self.url = url
+        if(self.text_type.value == TextType.LINK and self.url == None):
+            raise Exception("Links must have an URL")
     
     def __eq__(self, value):
         return self.text == value.text and self.text_type == value.text_type and self.url == value.url
