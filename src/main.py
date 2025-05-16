00289ff8f5e2d47c52367990867ac898014e7767
Daniel-Parada-da-Silva <daniel.parada.da.silva@gmail.com>
2025-05-16 16:49:53 +0200

    BlockType

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 from textnode import *
 from nodeconverter import NodeConverter
 
+from enum import Enum
+
 print("hello world")
+
+def BlockType(Enum):
+    PARAGRAPH = "Paragraph"
+    HEADING = "Heading"
+    CODE = "Code"
+    QUOTE = "Quote"
+    UL = "Unordered_List"
+    OL = "Ordered_List"
 
 def main():
     txtn = TextNode("This is some text", TextType.LINK, "https://www.boot.dev")
