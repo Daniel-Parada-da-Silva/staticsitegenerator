cff931718026d25914d8016b5e7dc5c2add0bd6a
Daniel-Parada-da-Silva <daniel.parada.da.silva@gmail.com>
2025-05-21 18:35:29 +0200

    Publish method generated

--- src/main.py
+++ src/main.py
@@ -1,12 +1,24 @@
+import os
+import shutil
+
 from textnode import *
 from blocknode import *
 from nodeconverter import *
 
-print("hello world")
+def publish(path: str):
+    lst = os.listdir(path)
+    new_path = path.replace("static", "public")
+    if not os.path.exists(new_path):
+        os.mkdir(new_path)
+    for item in lst:
+        current = path + item
+        if not os.path.isfile(current):
+            publish(current + "/")
+        else:
+            shutil.copy(current, current.replace("static", "public"))
 
 def main():
-    txtn = TextNode("This is some text", TextType.LINK, "https://www.boot.dev")
-    print(text_node_to_html_node(txtn))
-    print(txtn)
+    shutil.rmtree("./public/", ignore_errors = True)
+    publish("./static/")
 
 main()
